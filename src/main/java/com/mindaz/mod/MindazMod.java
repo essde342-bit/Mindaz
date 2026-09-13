305fcb220b09fcfa3fd473ee68228666545a4704
essde342-bit <essde342@gmail.com>
2026-09-13 21:31:34 +0300

    Update MindazMod.java

--- src/main/java/com/mindaz/mod/MindazMod.java
+++ src/main/java/com/mindaz/mod/MindazMod.java
@@ -1,19 +1,30 @@
 package com.mindaz.mod;
 
+import com.mojang.brigadier.CommandDispatcher;
+
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.minecraft.block.Blocks;
+
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
-import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryEntryLookup;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 
@@ -26,14 +37,14 @@ public class MindazMod implements ModInitializer {
     public static final String MOD_ID = "mindaz";
 
     public static final MindazItem MINDAZ = new MindazItem(
-            new net.minecraft.item.Item.Settings()
-                    .maxCount(1)
-                    .maxDamage(250)
+            new Item.Settings()
     );
 
-    private static final List<TemporaryBlock> TEMPORARY_BLOCKS = new ArrayList<>();
+    private static final List<TemporaryBlock> TEMPORARY_BLOCKS =
+            new ArrayList<>();
 
-    private static final int WOOL_TIME = 200;
+    // 10 секунд = 200 игровых тиков
+    private static final int WOOL_TIME = 10 * 20;
 
     @Override
     public void onInitialize() {
@@ -44,29 +55,84 @@ public class MindazMod implements ModInitializer {
                 MINDAZ
         );
 
+        // Обрабатываем исчезновение временной шерсти
         ServerTickEvents.END_SERVER_TICK.register(
                 MindazMod::tickTemporaryBlocks
         );
+
+        // Команда /mindaz
+        CommandRegistrationCallback.EVENT.register(
+                (dispatcher, registryAccess, environment) -> {
+                    registerCommands(
+                            dispatcher,
+                            registryAccess,
+                            environment
+                    );
+                }
+        );
     }
 
-    public static ItemStack createMindaz(MinecraftServer server) {
+    private static void registerCommands(
+            CommandDispatcher<ServerCommandSource> dispatcher,
+            CommandRegistryAccess registryAccess,
+            CommandManager.RegistrationEnvironment environment
+    ) {
+        dispatcher.register(
+                CommandManager.literal("mindaz")
+                        .executes(context -> {
 
+                            ServerPlayerEntity player =
+                                    context.getSource()
+                                            .getPlayerOrThrow();
+
+                            ItemStack stack =
+                                    createMindaz(
+                                            context.getSource()
+                                                    .getServer()
+                                    );
+
+                            player.getInventory()
+                                    .offerOrDrop(stack);
+
+                            context.getSource().sendFeedback(
+                                    () -> Text.literal(
+                                            "Mindaz получен!"
+                                    ),
+                                    false
+                            );
+
+                            return 1;
+                        })
+        );
+    }
+
+    public static ItemStack createMindaz(
+            MinecraftServer server
+    ) {
         ItemStack stack = new ItemStack(MINDAZ);
 
         RegistryEntryLookup<Enchantment> enchantments =
                 server.getRegistryManager()
-                        .getOrThrow(RegistryKeys.ENCHANTMENT);
+                        .getOrThrow(
+                                RegistryKeys.ENCHANTMENT
+                        );
 
         RegistryEntry<Enchantment> sharpness =
-                enchantments.getOrThrow(Enchantments.SHARPNESS);
+                enchantments.getOrThrow(
+                        Enchantments.SHARPNESS
+                );
 
         RegistryEntry<Enchantment> fireAspect =
-                enchantments.getOrThrow(Enchantments.FIRE_ASPECT);
+                enchantments.getOrThrow(
+                        Enchantments.FIRE_ASPECT
+                );
 
         RegistryEntry<Enchantment> unbreaking =
-                enchantments.getOrThrow(Enchantments.UNBREAKING);
+                enchantments.getOrThrow(
+                        Enchantments.UNBREAKING
+                );
 
-        net.minecraft.enchantment.EnchantmentHelper.apply(
+        EnchantmentHelper.apply(
                 stack,
                 builder -> {
                     builder.set(sharpness, 7);
@@ -82,7 +148,6 @@ public class MindazMod implements ModInitializer {
             ServerWorld world,
             BlockPos playerPos
     ) {
-
         BlockPos center = playerPos.down();
 
         for (int x = -1; x <= 1; x++) {
@@ -92,30 +157,31 @@ public class MindazMod implements ModInitializer {
             BlockState oldState =
                     world.getBlockState(pos);
 
-            if (oldState.isAir()) {
-
-                world.setBlockState(
-                        pos,
-                        Blocks.WHITE_WOOL.getDefaultState(),
-                        3
-                );
-
-                TEMPORARY_BLOCKS.add(
-                        new TemporaryBlock(
-                                world,
-                                pos,
-                                oldState,
-                                world.getTime() + WOOL_TIME
-                        )
-                );
+            // Не заменяем существующие блоки
+            if (!oldState.isAir()) {
+                continue;
             }
+
+            world.setBlockState(
+                    pos,
+                    Blocks.WHITE_WOOL.getDefaultState(),
+                    3
+            );
+
+            TEMPORARY_BLOCKS.add(
+                    new TemporaryBlock(
+                            world,
+                            pos,
+                            oldState,
+                            world.getTime() + WOOL_TIME
+                    )
+            );
         }
     }
 
     private static void tickTemporaryBlocks(
             MinecraftServer server
     ) {
-
         Iterator<TemporaryBlock> iterator =
                 TEMPORARY_BLOCKS.iterator();
 
@@ -125,21 +191,24 @@ public class MindazMod implements ModInitializer {
                     iterator.next();
 
             if (block.world().getTime()
-                    >= block.removeAtTick()) {
-
-                if (block.world()
-                        .getBlockState(block.pos())
-                        .isOf(Blocks.WHITE_WOOL)) {
-
-                    block.world().setBlockState(
-                            block.pos(),
-                            block.oldState(),
-                            3
-                    );
-                }
-
-                iterator.remove();
+                    < block.removeAtTick()) {
+                continue;
             }
+
+            // Возвращаем исходный блок только если
+            // шерсть всё ещё находится на этом месте.
+            if (block.world()
+                    .getBlockState(block.pos())
+                    .isOf(Blocks.WHITE_WOOL)) {
+
+                block.world().setBlockState(
+                        block.pos(),
+                        block.oldState(),
+                        3
+                );
+            }
+
+            iterator.remove();
         }
     }
 
@@ -148,5 +217,6 @@ public class MindazMod implements ModInitializer {
             BlockPos pos,
             BlockState oldState,
             long removeAtTick
-    ) {}
-      }
+    ) {
+    }
+}
